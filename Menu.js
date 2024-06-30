cb0b5d712172ec35171efabe586cb66e77ae91b9
Eric Ramos <54134375+fitfulg@users.noreply.github.com>
2024-06-30 11:01:10 +0200

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -2,7 +2,7 @@ function onOpen() {
     var ui = SpreadsheetApp.getUi();
     // Add a custom menu
     ui.createMenu('Custom Formats')
-        .addItem('Apply Format', 'applyFormat')
+        .addItem('Apply Format', 'applyFormatToSelected')
         .addItem('Apply Format to All', 'applyFormatToAll')
         .addItem('Set Ceil Background Colors from TODO sheet', 'customCeilBGColorTODO')
         .addItem('Log Hello World from github to googlesheets', 'logHelloWorld') // Añadir nueva función al menú
