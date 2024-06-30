c13d54f8b15f879b335c5743e18512ae1b36a3d3
Eric Ramos <54134375+fitfulg@users.noreply.github.com>
2024-06-30 11:00:21 +0200

    Update and rename Formatting.js to ceilFormat.js

--- ceilFormat.js
+++ ceilFormat.js
@@ -1,17 +1,17 @@
-function applyFormatting(range) {
+function Format(range) {
     // Apply the desired formats
     range.setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
     range.setHorizontalAlignment("center");
     range.setVerticalAlignment("middle");
 }
 
-function applyFormat() {
+function applyFormatToSelected() {
     // Get the active sheet and the selected range
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
     var range = sheet.getActiveRange();
 
     // Apply formatting to the selected range
-    applyFormatting(range);
+    Format(range);
 }
 
 function applyFormatToAll() {
@@ -20,5 +20,5 @@ function applyFormatToAll() {
     var range = sheet.getDataRange();
 
     // Apply formatting to the entire data range
-    applyFormatting(range);
+    Format(range);
 }
