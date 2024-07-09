05648a5ecfab44eb837adb68a1a2d3eb01bbc443
Eric <rc84.eric@gmail.com>
2024-07-09 08:42:28 +0200

    fix

--- ceilFormat.js
+++ ceilFormat.js
@@ -1,31 +1,39 @@
 const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
 
 function Format(range) {
-    // Apply the desired formats
-    range.setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
-    range.setHorizontalAlignment("center");
-    range.setVerticalAlignment("middle");
+    if (range) {
+        // Apply the desired formats
+        range.setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
+        range.setHorizontalAlignment("center");
+        range.setVerticalAlignment("middle");
+    }
 }
 
 function applyBorders(range) {
-    // Apply black borders with the thinnest line
-    range.setBorder(true, true, true, true, true, true, "#000000", SpreadsheetApp.BorderStyle.SOLID);
+    if (range) {
+        // Apply black borders with the thinnest line
+        range.setBorder(true, true, true, true, true, true, "#000000", SpreadsheetApp.BorderStyle.SOLID);
+    }
 }
 
 function applyFormatToSelected() {
     // Get the active sheet and the selected range
     let range = sheet.getActiveRange();
-    // Apply formatting to the selected range
-    Format(range);
-    applyBorders(range)
+    if (range) {
+        // Apply formatting to the selected range
+        Format(range);
+        applyBorders(range);
+    }
 }
 
 function applyFormatToAll() {
     // Get the active sheet and the entire data range
     let range = sheet.getDataRange();
-    // Apply formatting to the entire data range
-    Format(range);
-    applyBorders(range);
+    if (range) {
+        // Apply formatting to the entire data range
+        Format(range);
+        applyBorders(range);
+    }
 
     // Check the number of occupied cells in columns C, D, and E
     checkAndSetColumn("C", 10);
@@ -42,10 +50,12 @@ function checkAndSetColumn(column, limit) {
     if (occupied > limit) {
         // Set border color to red
         sheet.getRange(column + "2:" + column + dataRange.getLastRow()).setBorder(true, true, true, true, true, true, "#FF0000", SpreadsheetApp.BorderStyle.SOLID);
+        // Set cell value
         sheet.getRange(column + "1").setValue("⚠️limite de celdas alcanzadas⚠️");
     } else {
         // Set border color to black
         sheet.getRange(column + "2:" + column + dataRange.getLastRow()).setBorder(true, true, true, true, true, true, "#000000", SpreadsheetApp.BorderStyle.SOLID);
+        // Set cell value
         sheet.getRange(column + "1").setValue(column === "C" ? "PRIORIDAD ALTA" : column === "D" ? "PRIORIDAD MEDIA" : "PRIORIDAD BAJA");
     }
 }
