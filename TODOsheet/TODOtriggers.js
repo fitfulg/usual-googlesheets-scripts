235f97db9bc543802540194e591e210881a0acf9
Eric <rc84.eric@gmail.com>
2024-07-16 16:48:57 +0200

    fix onedit trigger

--- TODOsheet/TODOtriggers.js
+++ TODOsheet/TODOtriggers.js
@@ -5,16 +5,34 @@ function onEdit(e) {
     const column = range.getColumn();
     const row = range.getRow();
 
-    // Check if the edition is in columns C, D, E, F, G, H and from row 2 down
+    // Check if the edit is in columns C, D, E, F, G, H and from row 2 onwards
     if (column >= 3 && column <= 8 && row >= 2) {
         const cellValue = range.getValue();
         const date = new Date();
         const formattedDate = Utilities.formatDate(date, Session.getScriptTimeZone(), "dd/MM/yy");
 
-        // Formatear la fecha en cursiva y gris oscuro
-        const dateFormatted = ` ${formattedDate}`;
-        range.setValue(cellValue + dateFormatted)
+        // Create a RichTextValueBuilder to combine text and style
+        let richTextBuilder = SpreadsheetApp.newRichTextValue()
+            .setText(cellValue + ' ' + formattedDate);
+
+        // current text styles
+        const textStyle = SpreadsheetApp.newTextStyle()
+            .setFontStyle('normal')
+            .setForegroundColor('#000000')
+            .build();
+
+        richTextBuilder = richTextBuilder.setTextStyle(0, cellValue.length, textStyle);
+
+        // styles just for the date
+        const dateStyle = SpreadsheetApp.newTextStyle()
             .setFontStyle('italic')
-            .setFontColor('#555555');
+            .setForegroundColor('#555555')
+            .build();
+
+        richTextBuilder = richTextBuilder.setTextStyle(cellValue.length, cellValue.length + formattedDate.length + 1, dateStyle);
+
+        // Set the RichTextValue in the cell
+        range.setRichTextValue(richTextBuilder.build());
     }
 }
+
