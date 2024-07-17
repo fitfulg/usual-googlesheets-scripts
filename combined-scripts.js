24283ea25e6b1952de4ab0bc04a487f86bea61a2
Eric <rc84.eric@gmail.com>
2024-07-17 06:34:35 +0200

    add submenu for the TODO sheet

--- combined-scripts.js
+++ combined-scripts.js
@@ -4,13 +4,12 @@
 
 function onOpen() {
     let ui = SpreadsheetApp.getUi();
-    // Add a custom menu
+    // custom menu
     let todoSubMenu = ui.createMenu('TODO sheet')
         .addItem('Apply Format to All', 'applyFormatToAllTODO')
         .addItem('Set Ceil Background Colors', 'customCeilBGColorTODO')
         .addItem('Create Pie Chart', 'createPieChartTODO');
 
-    // Add the custom menu and the TODO sub-menu
     ui.createMenu('Custom Formats')
         .addItem('Apply Format', 'applyFormatToSelected')
         .addItem('Apply Format to All', 'applyFormatToAll')
