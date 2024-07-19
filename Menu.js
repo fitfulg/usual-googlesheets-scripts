5edf1e4655ed17b0360e28e52a45a7802e79bbe3
Eric <rc84.eric@gmail.com>
2024-07-19 11:36:43 +0200

    test

--- Menu.js
+++ Menu.js
@@ -23,6 +23,6 @@ function onOpen() {
 }
 
 function logHelloWorld() {
-    ui.alert('Hello World from Custom Menu!');
+    ui.alert('Hello World from Custom Menu!!!!!!!!!!!!');
     console.log('Hello World from Custom Menu!');
 }
