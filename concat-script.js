187ab971ed0b227124abe070e1de25bf3cacf362
Eric <rc84.eric@gmail.com>
2024-08-11 08:18:40 +0200

    test

--- concat-script.js
+++ concat-script.js
@@ -7,7 +7,7 @@
 const ui = SpreadsheetApp.getUi();
 const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
 const getDataRange = () => sheet.getDataRange();
-const datePattern = /\n\d{2}\/\d{2}\/\d{2}$/; // dd/MM/yy
+const datePattern = /\d{2}\/\d{2}\/\d{2}$/;
 
 // state management
 let isPieChartVisible = false;
