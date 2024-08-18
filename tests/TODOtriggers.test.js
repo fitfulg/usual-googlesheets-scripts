31ef1bd0d59603e8d86fbf37db781707603dc876
Eric <rc84.eric@gmail.com>
2024-08-18 07:08:13 +0200

    remove test

--- tests/TODOtriggers.test.js
+++
@@ -1,7 +0,0 @@
-const TODOtriggers = require('../TODOsheet/TODOtriggers');
-
-describe('TODOtriggers Functions', () => {
-    test('onEdit function exists', () => {
-        expect(typeof TODOtriggers.onEdit).toBe('function');
-    });
-});
